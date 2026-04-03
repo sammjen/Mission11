bbc63218e53210e22383934098ae3a3a7ef81fb5
sammjen <samjen@byu.edu>
2026-04-03 17:52:02 -0600

    final changes

--- backend/Mission11/Mission11/obj/Release/net8.0/Mission11.AssemblyInfo.cs
+++ backend/Mission11/Mission11/obj/Release/net8.0/Mission11.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3f5fedcee478f999f896fb8d8b7a81fb669d2c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1046a5f895706d0b9f941489dc8a03f8b33fd42b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
